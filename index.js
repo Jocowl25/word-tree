f9c6527749293ea6d8577502afb755754b96d2c8
jocowl <jocowl25@gmail.com>
2025-07-30 12:11:10 -0400

    add punctuation detection

--- index.js
+++ index.js
@@ -2,16 +2,27 @@ const inputEle=document.querySelector('input')
 const go=document.querySelector('.go')
 const copy=document.querySelector('.copy')
 const output=document.querySelector('.output')
+const charset="qwertyuiopasdfghjklzxcvbnm1234567890".split("")
+const precedingchars=` "(`.split("") //set of characters that are connected to the next character in the tree
 go.addEventListener("click",()=>{
     output.innerHTML=""
     const input=inputEle.value
     let inputArr=input.split("")
     for(let i=1;i<inputArr.length+1;i++){
-    let newArr=inputArr.toSpliced(i)
-    if(newArr[newArr.length-1]!=" "){
-        //????
-    output.innerHTML+=newArr.join("")+'<br>'
-    }
+        let newArr=inputArr.toSpliced(i)
+        let additChar=""
+        const currentChar=newArr[newArr.length-1]
+        const nextChar=inputArr[i]
+        const prevChar=inputArr[i-2]
+        if(!charset.includes(currentChar.toLowerCase())){
+            if(precedingchars.includes(currentChar)||(i>2&&charset.includes(prevChar.toLowerCase()))){
+            continue
+            }
+        }
+        else if(!precedingchars.includes(currentChar)&&i<inputArr.length&&!charset.includes(nextChar.toLowerCase())){
+            additChar=nextChar
+        }
+        output.innerHTML+=newArr.join("")+additChar+'<br>'
     }
 })
 copy.addEventListener("click",()=>{
